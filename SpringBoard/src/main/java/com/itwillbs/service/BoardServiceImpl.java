99b46dbe71d4e2b549e0e1a8b386eddeea05d106
Yonadada <yknda123@gmail.com>
2024-06-10 11:00:17 +0900

    #7 게시판 글쓰기 서비스 작업 완료

---
+++ SpringBoard/src/main/java/com/itwillbs/service/BoardServiceImpl.java
@@ -0,0 +1,29 @@
+package com.itwillbs.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.domain.BoardVO;
+import com.itwillbs.persistence.BoardDAO;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardServiceImpl.class);
+
+	//DAO 객체를 주입 
+	private BoardDAO bdao;
+	
+	@Override
+	public void regist(BoardVO vo) throws Exception {
+		logger.debug("연결된 DAO 메서드를 호출");
+	
+		bdao.create(vo);
+		
+		logger.debug("글쓰기 완료!");
+	
+	}
+	
+	
+}
